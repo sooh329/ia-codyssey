a65616b9fd94daf1cb5350fff5b9411004f7acb7
sooh329 <glay@kakao.com>
2026-04-02 13:35:39 +0900

    docs: 함수 설명 주석 수정 및 세부 설명 추가

--- 0402/mars_mission_computer.py
+++ 0402/mars_mission_computer.py
@@ -3,21 +3,21 @@ import time
 
 
 class DummySensor:
-    """테스트용 더미 환경 센서(화성 기지 지표를 난수로 생성)."""
+    """테스트용 더미 환경 센서"""
 
     def __init__(self):
-        """인스턴스를 만들고 env_values 사전 키를 준비한다."""
+        """인스턴스를 만들고 env_values 사전 키를 준비한다"""
         self.env_values = {
-            'mars_base_internal_temperature': None,
-            'mars_base_external_temperature': None,
-            'mars_base_internal_humidity': None,
-            'mars_base_external_illuminance': None,
-            'mars_base_internal_co2': None,
-            'mars_base_internal_oxygen': None,
+            'mars_base_internal_temperature': None, # 화성 기지 내부 온도
+            'mars_base_external_temperature': None, # 화성 기지 외부 온도
+            'mars_base_internal_humidity': None, # 화성 기지 내부 습도
+            'mars_base_external_illuminance': None, # 화성 기지 외부 광량
+            'mars_base_internal_co2': None, # 화성 기지 내부 이산화탄소 농도
+            'mars_base_internal_oxygen': None, # 화성 기지 내부 산소 농도
         }
 
     def set_env(self):
-        """random으로 각 센서 범위의 값을 생성해 env_values에 채운다."""
+        """random으로 각 센서 범위의 값을 생성해 env_values에 채운다"""
         self.env_values['mars_base_internal_temperature'] = round(
             random.uniform(18, 30), 2
         )
@@ -27,7 +27,9 @@ class DummySensor:
         self.env_values['mars_base_internal_humidity'] = round(
             random.uniform(50, 60), 2
         )
-        self.env_values['mars_base_external_illuminance'] = random.randint(500, 715)
+        self.env_values['mars_base_external_illuminance'] = round(
+            random.randint(500, 715), 2
+        )
         self.env_values['mars_base_internal_co2'] = round(
             random.uniform(0.02, 0.1), 4
         )
@@ -36,8 +38,8 @@ class DummySensor:
         )
 
     def get_env(self):
-        """현재 env_values 사전을 반환한다."""
-        # 측정 시각과 각 항목을 로그 파일에 남긴다.
+        """현재 env_values 사전을 반환한다"""
+        # 측정 시각과 각 항목을 로그 파일에 남긴다
         log_path = 'mars_base_environment.log'
         timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
         line = (
@@ -55,7 +57,7 @@ class DummySensor:
         return self.env_values
 
 
-# DummySensor를 ds 인스턴스로 만들고 set_env·get_env를 차례로 호출해 값을 확인한다.
+# DummySensor를 ds 인스턴스로 만들고 set_env, get_env를 차례로 호출해 값을 확인한다
 ds = DummySensor()
 ds.set_env()
 print(ds.get_env())
